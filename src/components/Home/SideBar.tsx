e8986909b0e58b860fc752f1e08f9c22909b7785
yangzheng <snitchyang@qq.com>
2023-06-25 15:21:44 +0800

    Navigation added

--- src/components/Home/SideBar.tsx
+++ src/components/Home/SideBar.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { BulbOutlined, TagsOutlined, UserOutlined  } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
+import {useNavigate} from "react-router-dom";
 
 type MenuItem = Required<MenuProps>['items'][number];
+
 
 function getItem(
   label: React.ReactNode,
@@ -23,23 +25,27 @@ function getItem(
 
 const items:MenuProps['items'] = [
   getItem('所有问题', 'all', <BulbOutlined />, [
-    getItem('全部问题', 'allQuestions'),
+    getItem('全部问题', 'questions'),
     getItem('未回答的', 'unanswered'),
-    getItem('我的问题', 'myQuestions'),
+    getItem('我的问题', 'myQuestions', null, [
+      getItem('我回答的', 'myAnswers'),
+      getItem('我提出的', 'myAsked'),
+    ]),
   ]),
   getItem('标签', 'tags', <TagsOutlined />),
   getItem('用户', 'users', <UserOutlined />),
 ]
 
 export const SideBar: React.FC = () => {
+  const navigate = useNavigate();
   const onClick: MenuProps['onClick'] = (e) => {
     console.log('click ', e);
+    navigate('/' + e.key);
   };
 
   return (
     <Menu
       onClick={onClick}
-      style={{ width: 256 }}
       defaultSelectedKeys={['1']}
       defaultOpenKeys={['sub1']}
       mode="inline"
