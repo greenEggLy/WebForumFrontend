e2fa682553f0c6a61acf26444bcad3cf849e3983
starCloudXY <z18724628341@outlook.com>
2023-06-26 08:47:23 +0800

    注册与登录界面（未链接后端版本）

---
+++ src/constants/color.ts
@@ -0,0 +1,10 @@
+export default {
+    primary: "black",
+    gray: "#666666",
+    black: "#000000",
+    white: "#FFFFFF",
+    dgrayblue:"#4b6584",
+    lgrayblue:"#778ca3",
+    card: "white",
+    warning: "#E03D2E",
+};
