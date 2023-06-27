4f34eac2629bc4041478558853580cc7e67f3463
starCloudXY <z18724628341@outlook.com>
2023-06-27 08:56:49 +0800

    用户usecard界面美化（和后端连接时需要取消注释,将css样式提到了global.tsx里

---
+++ src/constants/test.ts
@@ -0,0 +1,33 @@
+import {ITag} from "../Interface.ts";
+import {IUserCard} from "../Interface.ts";
+const tag1:ITag={
+    id:1,
+    content:"Coding"
+}
+const tag2:ITag={
+    id:2,
+    content:"计算机"
+}
+const tag3:ITag={
+    id:3,
+    content:"软件"
+}
+const User1:IUserCard={
+    id:1,
+    avatar:"https://tse2-mm.cn.bing.net/th/id/OIP-C.PcOT-lN3-IAT3szcXiXvQgAAAA?w=186&h=186&c=7&r=0&o=5&dpr=2&pid=1.7",
+    username:"沈备军",
+    location: "中国上海",
+    followers_number: 233,
+    profile: "一名软件学院老师",
+    fields: [tag1,tag2,tag3]
+}
+const User2:IUserCard={
+    id:2,
+    avatar:"https://tse2-mm.cn.bing.net/th/id/OIP-C.PcOT-lN3-IAT3szcXiXvQgAAAA?w=186&h=186&c=7&r=0&o=5&dpr=2&pid=1.7",
+    username:"沈备军",
+    location: "中国上海",
+    followers_number: 233,
+    profile: "一名软件学院老师",
+    fields: [tag1,tag2,tag3]
+}
+export const UserList:IUserCard[]=[User1,User2];
