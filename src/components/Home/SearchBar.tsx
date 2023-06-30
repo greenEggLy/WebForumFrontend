10e751e7c00a352dcc413ad2c690b123626f1298
yangzheng <snitchyang@qq.com>
2023-06-30 08:58:54 +0800

    searchBox modified

--- src/components/Home/SearchBar.tsx
+++ src/components/Home/SearchBar.tsx
@@ -1,38 +1,41 @@
-import {SearchOutlined} from "@ant-design/icons";
-import {Input} from "antd";
 import "./SearchBar.css"
 import logo from "../../assets/logos/logo.png"
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store.ts";
 import {changeKeyword} from "../../features/keyword/keywordSlice.ts";
+import { SearchBox } from "./SearchBox.tsx";
 
 export const SearchBar = () => {
 	const keyword = useSelector((state: RootState) => state.keyword.value);
 	const dispatch = useDispatch()
 
 	return (
-		<div className={'top-search-bar-container'}>
-			<img src={logo} className={'logo'} style={{width: '7rem', margin: '0.5rem'}}/>
-			<Input style={styles.search} prefix={<SearchOutlined/>} value={keyword} onChange={(event) => {
-				dispatch(changeKeyword(event.target.value))
-			}}/>
+		<div className={'top-search-bar-container'} >
+			<div className={'top-logo-container'}>
+				<img src={logo} className={'logo'} style={{width:'7rem', margin:'0.5rem'}}/>
+			</div>
+			<div className={'top-search-box-container'}>
+				<SearchBox placeholder={'Search'} onSearch={(s:string) => dispatch(changeKeyword(s))}/>
+			</div>
 			<div className={'logout-button-container'}>
-				<a href={'/login'} style={{position: 'relative', top: '0.5rem', color: "white"}}>登出</a>
+				<div className={'logout-button'}>
+					<a href={'/login'} style={{position:'relative' ,top:'0.25rem', color:"white"}}>登出</a>
+				</div>
 			</div>
 		</div>
 	);
 }
 
-const styles = {
-	searchBar: {
-		height: '100%',
-		backgroundColor: 'black',
-	},
-	search: {
-		marginTop: '0.65rem',
-		width: '50vw',
-		marginLeft: '10vw',
-		top: '-1.5rem',
-		borderRadius: '1rem',
-	}
-}
+// const styles = {
+// 	searchBar: {
+// 		height: '100%',
+// 		backgroundColor: 'black',
+// 	},
+// 	search: {
+// 		marginTop: '0.65rem',
+// 		width: '50vw',
+// 		marginLeft: '10vw',
+// 		top: '-1.5rem',
+// 		borderRadius: '1rem',
+// 	}
+// }
--- src/views/TagsView.tsx
+++ src/views/TagsView.tsx
@@ -5,7 +5,6 @@ import {TagCard} from "../components/tag/TagCard.tsx";
 import {Tag_GetAllTags} from "../service/TagService.ts";
 import {tag1, tag2, tag3} from "../constants/test"
 import './css/TagsView.css'
-import Search from "antd/es/input/Search";
 import { SearchBox } from "../components/Home/SearchBox.tsx";
 
 export const TagsView = () => {
@@ -25,22 +24,23 @@ export const TagsView = () => {
 		}
 		setTags(await response.json());
 	}
-	return (<div>
-		<div className={"title-container"}>
-			<h2>Tags</h2>
-		</div>
-		<div className={'tag-search-bar-container'}>
-			{/*<Search placeholder={"Search tags"}/>*/}
-			<SearchBox placeholder={'search'} onSearch={(s:string) => console.log(s)}/>
-		</div>
-		<div className={'tag-card-container'}>
-			<List
-				grid={{gutter: 16, column: 4}}
-				dataSource={tags}
-				renderItem={item => <TagCard tag={item}/>}
-				style={{marginLeft: '1rem'}}
-			>
-			</List>
-		</div>
-	</div>);
+	return (
+		<div className={"tags-view-container"}>
+			<div className={"title-container"}>
+				<h2>Tags</h2>
+			</div>
+			<div className={'tag-search-bar-container'}>
+				{/*<Search placeholder={"Search tags"}/>*/}
+				<SearchBox placeholder={'search'} onSearch={(s:string) => console.log(s)}/>
+			</div>
+			<div className={'tag-card-container'}>
+				<List
+					grid={{gutter: 16, column: 4}}
+					dataSource={tags}
+					renderItem={item => <TagCard tag={item}/>}
+					style={{marginLeft: '1rem'}}
+				>
+				</List>
+			</div>
+		</div>);
 };
